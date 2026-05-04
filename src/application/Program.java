88bebc839bb990bba2e794c237ee2559077973cd
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-05-03 22:59:04 -0300

    Adicionei o layout do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
         Board board = new Board (8,8);
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(ChessMatch.getPices());
+        UI.printBoard(chessMatch.getPieces());
 
 
     }
